d23bdfb97a6c77169b2436fdb55a76e79b77b6c8
doron nahum <doron.nahum@gmail.com>
2019-11-28 10:07:15 +0200

    "version": "1.7.1", fix style issues and allow to pass dateFormat to date list field

--- src/helpers/getListField.js
+++ src/helpers/getListField.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 import React from 'react';
 import startCase from 'lodash/startCase';
 import { Tag, Tooltip } from 'antd';
@@ -6,7 +7,7 @@ import printObject from './printObject';
 
 import { LOCALS } from '../local';
 
-const getListField = function ({ key, title, type, render, sorter, width = 150, defaultValue = '', itemType, labelKey }) {
+const getListField = function ({ key, title, type, render, sorter, width = 150, defaultValue = '', itemType, labelKey, dateFormat }) {
   const field = {
     title: title || startCase(key),
     key,
@@ -19,10 +20,10 @@ const getListField = function ({ key, title, type, render, sorter, width = 150, 
   if (render) {
     field.render = render;
   } else if (type === Array || type === 'array') {
-      field.render = function (fieldValue) {
-        if (fieldValue && fieldValue.length) {
- return (
-          <div className="ra-max100">
+    field.render = function (fieldValue) {
+      if (fieldValue && fieldValue.length) {
+        return (
+          <div className="ra-listTags">
             {
               fieldValue.map((val, index) => {
                 if (itemType === Object || (val && typeof val === 'object')) return <Tag key={index} style={{ height: 'auto' }} color="geekblue">{labelKey ? val[labelKey] : printObject(val)}</Tag>;
@@ -31,49 +32,49 @@ const getListField = function ({ key, title, type, render, sorter, width = 150, 
             }
           </div>
         );
-}
-        return defaultValue;
-      };
-    } else if (type === Date || type === 'date') {
-      field.width = field.width || 100;
-      field.render = function (fieldValue) { return fieldValue ? moment(fieldValue).format(LOCALS.TABLE_DATE_FIELD_FORMAT) : defaultValue; };
-    } else if (type === Boolean || type === 'boolean') {
-      field.width = field.width || 80;
-      field.render = function (fieldValue) { return fieldValue ? <span>&#10004;</span> : <span>&#10008;</span>; };
-    } else if (type === Object || type === 'object') {
-      if (!labelKey) {
-        field.render = function (fieldValue) {
-          if (typeof fieldValue === 'string') return fieldValue;
-          return JSON.stringify(fieldValue || {});
-        };
-      } else {
-        field.render = function (fieldValue) {
-          if (typeof fieldValue === 'string') return fieldValue;
-          return fieldValue ? fieldValue[labelKey] : (fieldValue || '');
-        };
       }
-    } else if (type === 'link') {
+      return defaultValue;
+    };
+  } else if (type === Date || type === 'date') {
+    field.width = field.width || 100;
+    field.render = function (fieldValue) { return fieldValue ? moment(fieldValue).format(dateFormat || LOCALS.TABLE_DATE_FIELD_FORMAT) : defaultValue; };
+  } else if (type === Boolean || type === 'boolean') {
+    field.width = field.width || 80;
+    field.render = function (fieldValue) { return fieldValue ? <span>&#10004;</span> : <span>&#10008;</span>; };
+  } else if (type === Object || type === 'object') {
+    if (!labelKey) {
       field.render = function (fieldValue) {
-        return fieldValue ? (
-<a
-className="ra-linkStyle ra-list-text-ellipsis"
-onClick={(e) => {
-          e.stopPropagation();
-          window.open(fieldValue);
-        }}
->{fieldValue}
-</a>
-) : '';
+        if (typeof fieldValue === 'string') return fieldValue;
+        return JSON.stringify(fieldValue || {});
       };
-    } else if (field.key === '_id') {
-      field.width = 95;
+    } else {
       field.render = function (fieldValue) {
-        if (!fieldValue) return;
-        const str = fieldValue;
-        const start = fieldValue.length - 6;
-        return <Tooltip title={fieldValue}>{str.slice(start > 0 ? start : 0, fieldValue.length)}</Tooltip>;
+        if (typeof fieldValue === 'string') return fieldValue;
+        return fieldValue ? fieldValue[labelKey] : (fieldValue || '');
       };
     }
+  } else if (type === 'link') {
+    field.render = function (fieldValue) {
+      return fieldValue ? (
+        <a
+          className="ra-linkStyle ra-list-text-ellipsis"
+          onClick={(e) => {
+            e.stopPropagation();
+            window.open(fieldValue);
+          }}
+        >{fieldValue}
+        </a>
+      ) : '';
+    };
+  } else if (field.key === '_id') {
+    field.width = 95;
+    field.render = function (fieldValue) {
+      if (!fieldValue) return;
+      const str = fieldValue;
+      const start = fieldValue.length - 6;
+      return <Tooltip title={fieldValue}>{str.slice(start > 0 ? start : 0, fieldValue.length)}</Tooltip>;
+    };
+  }
   return field;
 };
 
